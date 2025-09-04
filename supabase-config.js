9ce657479fe8283195bb0b3b4083050b7e80b011
J-zavala-c <julietazavalaoficial@gmail.com>
2025-09-04 19:58:42 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// Configuración de Supabase
+export const SUPABASE_URL = "https://tpcpxykwaivcztfkrthg.supabase.co";
+export const SUPABASE_KEY = "TeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwY3B4eWt3YWl2Y3p0ZmtydGhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMjAwMTcsImV4cCI6MjA3MjU5NjAxN30.EIT-6eE7hkxBFqzk1AVawcA-IJ8lvjZ0O_eq6ZvG9oA";
